2ed9493cc929a95814f295b852a8d589538a8120
Misha Wagner <mishajw@gmail.com>
2018-02-04 18:18:54 +0000

    Add part 2 with setting up iptables to block port 80 on server with tests

---
+++ part2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+start_session
+
+echo Blocking server:80
+container_run server \
+  "iptables --append INPUT --protocol tcp --destination-port 80 -j DROP"
+
+echo Checking ssh from client
+RESPONSE=$(container_run client "$sshq server echo -n success")
+if [ "$RESPONSE" != "success" ]; then
+  echo Failed to ssh from client to server
+  exit
+fi
+
+# Checks if curl response contains 28, the error code for timeout
+echo Checking curl from client
+RESPONSE=$(container_run client "curl server --connect-timeout 1")
+if [[ "$RESPONSE" != *"28"* ]]; then
+  echo Client could still curl from server
+fi
+
+echo Success
+
+end_session
+
