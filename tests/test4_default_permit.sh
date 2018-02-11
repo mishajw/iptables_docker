e5e0d154cc7c977b77ac68874014a5ee69ea03a0
Misha Wagner <mishajw@gmail.com>
2018-02-11 10:58:19 +0000

    Rename part4_server to part4_default_permit

--- tests/test4_default_permit.sh
+++ tests/test4_default_permit.sh
@@ -5,7 +5,7 @@ source common.sh
 start_session
 
 echo Blocking all traffic from server to client on port 80 on router
-container_run_file router part4_server.sh
+container_run_file router part4_default_permit.sh
 
 check_client_ssh_server
 check_client_not_curl_server
